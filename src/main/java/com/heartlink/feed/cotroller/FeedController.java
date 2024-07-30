5b622704d4146e61df6f3f1db3e0251897c8f8ed
JaeIn <jein5874@gmail.com>
2024-07-30 12:51:01 +0900

    add : feed 페이지 생성 및 경로연동

---
+++ src/main/java/com/heartlink/feed/cotroller/FeedController.java
@@ -0,0 +1,15 @@
+package com.heartlink.feed.cotroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/feed")
+public class FeedController {
+
+    @GetMapping("")
+    public String feed() {
+        return "/feed/feedmain";  // "index"는 src/main/resources/templates/index.html
+    }
+}
