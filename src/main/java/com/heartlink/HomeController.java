6b2d3e231dfad360933d66cdeda00d775d16e4eb
JaeIn <jein5874@gmail.com>
2024-07-30 11:44:37 +0900

    add : css,js 및 기본 파일구조 추가

---
+++ src/main/java/com/heartlink/HomeController.java
@@ -0,0 +1,13 @@
+package com.heartlink;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";  // "index"는 src/main/resources/templates/index.html
+    }
+}
