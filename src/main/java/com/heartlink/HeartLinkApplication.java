a763456511b258232ccde63ebe1a09064b07c99e
JaeIn <jein5874@gmail.com>
2024-07-30 10:25:01 +0900

    project create

---
+++ src/main/java/com/heartlink/HeartLinkApplication.java
@@ -0,0 +1,13 @@
+package com.heartlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartLinkApplication.class, args);
+    }
+
+}
