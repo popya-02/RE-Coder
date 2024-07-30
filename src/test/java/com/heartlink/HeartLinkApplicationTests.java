c9edfc456d1c2619f153a4e1627b9e94a2fe2e02
JaeIn <jein5874@gmail.com>
2024-07-30 10:25:01 +0900

    project create

---
+++ src/test/java/com/heartlink/HeartLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
