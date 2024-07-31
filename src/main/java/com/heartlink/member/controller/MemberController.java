078d4f061c85020383b1af766f00ffbb2051e8c6
eunsik jung <bamtol2100@naver.com>
2024-07-31 16:50:42 +0900

    feat : 로그인/회원가입 페이지 프론트 작업중

---
+++ src/main/java/com/heartlink/member/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.heartlink.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping("/sign")
+    public String signInUp() {
+        return "member/sign_in_up"; // templates/member/sign_in_up.html을 렌더링
+    }
+}
