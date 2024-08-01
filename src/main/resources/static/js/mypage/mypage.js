f3e80c01165acaf7afef98e60225e0450293ba5b
PurPleChEEzE <ahssa96@gmail.com>
2024-08-01 10:29:16 +0900

    feat : 안내 메시지 추가 / 항목 추가

--- src/main/resources/static/js/mypage/mypage.js
+++ src/main/resources/static/js/mypage/mypage.js
@@ -1,23 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const maxSelections = 3;
+    const maxSentiSelections = 3;
+    const maxHobbySelections = 5;
 
+    // 성향 체크박스 처리
     const likeCheckboxes = document.querySelectorAll('input[name="likes"]');
     const dislikeCheckboxes = document.querySelectorAll('input[name="dislikes"]');
 
     likeCheckboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
-            if (document.querySelectorAll('input[name="likes"]:checked').length > maxSelections) {
+            if (document.querySelectorAll('input[name="likes"]:checked').length > maxSentiSelections) {
                 checkbox.checked = false;
-                alert(`최대 ${maxSelections}개까지 선택할 수 있습니다.`);
+                alert(`최대 ${maxSentiSelections}개까지 선택할 수 있습니다.`);
             }
         });
     });
 
     dislikeCheckboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
-            if (document.querySelectorAll('input[name="dislikes"]:checked').length > maxSelections) {
+            if (document.querySelectorAll('input[name="dislikes"]:checked').length > maxSentiSelections) {
                 checkbox.checked = false;
-                alert(`최대 ${maxSelections}개까지 선택할 수 있습니다.`);
+                alert(`최대 ${maxSentiSelections}개까지 선택할 수 있습니다.`);
+            }
+        });
+    });
+
+    // 취미 체크박스 처리
+    const hobbyCheckboxes = document.querySelectorAll('input[name="hobbies"]');
+
+    hobbyCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            if (document.querySelectorAll('input[name="hobbies"]:checked').length > maxHobbySelections) {
+                checkbox.checked = false;
+                alert(`취미는 최대 ${maxHobbySelections}개까지 선택할 수 있습니다.`);
             }
         });
     });
