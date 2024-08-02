a816ded5384738ebf40964ffb3a6250410bae484
JaeIn <jein5874@gmail.com>
2024-08-02 15:22:01 +0900

    fix : 충돌 코드 수정

---
+++ src/main/java/com/heartlink/charge/contoller/ChargeController.java
@@ -0,0 +1,20 @@
+package com.heartlink.charge.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/charge")
+public class ChargeController {
+
+    @GetMapping("/shop")
+    public String moveMain(){
+        return "coin_charge/charge-main";
+    }
+
+    @GetMapping("/history")
+    public String cashpage() {
+        return "coin_charge/mypage-cash";
+    }
+}
